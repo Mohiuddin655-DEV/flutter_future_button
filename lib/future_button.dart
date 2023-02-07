8bfd720c19ed1aa703c3ef6adaff67db775a7180
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-02-07 18:16:05 +0600

    v-1.0.1

--- lib/future_button.dart
+++ lib/future_button.dart
@@ -1,30 +1,53 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 class FutureButton extends StatefulWidget {
-  final String text;
-  final Color? primary;
   final double? width, height;
-  final double? borderRadius;
-  final EdgeInsetsGeometry? padding, margin;
+  final EdgeInsetsGeometry? margin, padding;
+  final String? text;
+  final double? textSize;
+  final FontWeight? textStyle;
+  final double borderRadius;
   final bool enabled;
-  final Function()? onPressed;
-  final Future<bool> Function()? onExecute;
+  final Function()? onClick;
+  final Future<dynamic> Function()? onExecute;
+  final IconData? icon;
+  final double iconSize;
+  final bool expended;
+  final EdgeInsetsGeometry? iconPadding;
+  final IconAlignment iconAlignment;
+  final bool visibleIndicator;
+
+  final bool? Function(ButtonState state)? expendedState;
+  final String? Function(ButtonState state)? textState;
+  final IconData? Function(ButtonState state)? iconState;
+  final Color? Function(ButtonState state)? colorState;
+  final Color? Function(ButtonState state)? backgroundState;
 
   const FutureButton({
-    Key? key,
-    required this.text,
-    this.primary,
+    super.key,
+    this.text,
+    this.textSize = 16,
+    this.textStyle,
     this.width,
     this.height,
-    this.padding,
     this.margin,
-    this.borderRadius,
+    this.padding,
+    this.borderRadius = 0,
     this.enabled = true,
-    this.onPressed,
+    this.onClick,
     this.onExecute,
-  }) : super(key: key);
+    this.icon,
+    this.expended = false,
+    this.iconSize = 18,
+    this.iconPadding,
+    this.iconAlignment = IconAlignment.end,
+    this.visibleIndicator = true,
+    this.expendedState,
+    this.textState,
+    this.iconState,
+    this.colorState,
+    this.backgroundState,
+  });
 
   @override
   State<FutureButton> createState() => _FutureButtonState();
@@ -33,35 +56,70 @@ class FutureButton extends StatefulWidget {
 class _FutureButtonState extends State<FutureButton> {
   late bool isLoaded = true;
 
-  late Color primary = widget.primary ?? Theme.of(context).primaryColor;
-
   @override
   Widget build(BuildContext context) {
-    final color = isLoaded ? Colors.white : Colors.black.withOpacity(0.4);
-    final background = isLoaded ? primary : primary.withOpacity(0.1);
+    final theme = Theme.of(context);
+    final color = isEnabled ? Colors.white : Colors.grey.shade400;
+    final background = isEnabled ? theme.primaryColor : Colors.grey.shade200;
 
-    return GestureDetector(
-      onTap: (widget.onPressed != null || widget.onExecute != null) && isLoaded
-          ? _onClick
-          : null,
-      child: Container(
-        margin: widget.margin ?? const EdgeInsets.symmetric(vertical: 8),
+    return Container(
+      margin: widget.margin,
+      child: Material(
+        color: widget.backgroundState?.call(state) ?? background,
         clipBehavior: Clip.antiAlias,
-        width: widget.width,
-        height: widget.height,
-        padding: widget.padding,
-        decoration: BoxDecoration(
-          color: background,
-          borderRadius: BorderRadius.circular(widget.borderRadius ?? 0),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-          child: Text(
-            widget.text,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: color,
-              fontSize: 18,
+        borderRadius: BorderRadius.circular(widget.borderRadius),
+        child: InkWell(
+          onTap: isEnabled && isLoaded ? _onClick : null,
+          child: AbsorbPointer(
+            child: Container(
+              width: widget.width,
+              height: widget.padding == null ? widget.height : null,
+              padding: widget.padding ??
+                  const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 12,
+                  ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FBIcon(
+                    visible: isIconAvailable &&
+                        widget.iconAlignment == IconAlignment.start,
+                    state: state,
+                    icon: widget.icon,
+                    iconState: widget.iconState,
+                    color: color,
+                    colorState: widget.colorState,
+                    size: widget.iconSize,
+                    padding: widget.iconPadding,
+                  ),
+                  if (isExpended && widget.iconAlignment == IconAlignment.start)
+                    const Spacer(),
+                  FBText(
+                    state: state,
+                    primary: color,
+                    text: widget.text,
+                    textSize: widget.textSize,
+                    textStyle: widget.textStyle,
+                    textState: widget.textState,
+                    colorState: widget.colorState,
+                  ),
+                  if (isExpended && widget.iconAlignment == IconAlignment.end)
+                    const Spacer(),
+                  FBIcon(
+                    visible: isIconAvailable &&
+                        widget.iconAlignment == IconAlignment.end,
+                    state: state,
+                    icon: widget.icon,
+                    iconState: widget.iconState,
+                    color: color,
+                    colorState: widget.colorState,
+                    size: widget.iconSize,
+                    padding: widget.iconPadding,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -69,12 +127,21 @@ class _FutureButtonState extends State<FutureButton> {
     );
   }
 
+  bool get isIconAvailable => (widget.iconState ?? widget.icon) != null;
+
+  bool get isEnabled =>
+      (widget.onClick != null || widget.onExecute != null) && widget.enabled;
+
+  bool get isExpended =>
+      (widget.iconState ?? widget.icon) != null &&
+      (widget.expendedState?.call(state) ?? widget.expended);
+
   void _onClick() async {
     loading;
     if (widget.onExecute != null) {
       await widget.onExecute?.call();
     } else {
-      widget.onPressed?.call();
+      widget.onClick?.call();
     }
     loaded;
   }
@@ -82,4 +149,112 @@ class _FutureButtonState extends State<FutureButton> {
   get loading => setState(() => isLoaded = false);
 
   get loaded => setState(() => isLoaded = true);
+
+  ButtonState get state {
+    if (isEnabled) {
+      if (isLoaded) {
+        return ButtonState.initial;
+      } else {
+        return ButtonState.loading;
+      }
+    } else {
+      return ButtonState.disabled;
+    }
+  }
+}
+
+class FBIcon extends StatelessWidget {
+  final ButtonState state;
+  final IconData? icon;
+  final bool visible;
+  final EdgeInsetsGeometry? padding;
+  final Color? color;
+  final double? size;
+  final IconData? Function(ButtonState state)? iconState;
+  final Color? Function(ButtonState state)? colorState;
+  final bool indicatorVisible;
+
+  const FBIcon({
+    Key? key,
+    required this.state,
+    this.icon,
+    this.visible = true,
+    this.padding,
+    this.color,
+    this.size,
+    this.iconState,
+    this.colorState,
+    this.indicatorVisible = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final c = colorState?.call(state) ?? color;
+    return Visibility(
+      visible: visible,
+      child: Container(
+        padding: padding,
+        child: indicatorVisible && state == ButtonState.loading
+            ? SizedBox(
+                width: size,
+                height: size,
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.transparent,
+                  strokeWidth: (size ?? 0) * 0.12,
+                  color: c,
+                ),
+              )
+            : Icon(
+                iconState?.call(state) ?? icon,
+                color: c,
+                size: size,
+              ),
+      ),
+    );
+  }
+}
+
+class FBText extends StatelessWidget {
+  final Color? primary;
+  final String? text;
+  final double? textSize;
+  final FontWeight? textStyle;
+  final String? Function(ButtonState state)? textState;
+  final Color? Function(ButtonState state)? colorState;
+  final ButtonState state;
+
+  const FBText({
+    Key? key,
+    required this.state,
+    this.primary,
+    this.text,
+    this.textSize = 14,
+    this.textStyle,
+    this.textState,
+    this.colorState,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      textState?.call(state) ?? text ?? "",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: colorState?.call(state) ?? primary,
+        fontSize: textSize,
+        fontWeight: textStyle,
+      ),
+    );
+  }
+}
+
+enum IconAlignment {
+  start,
+  end,
+}
+
+enum ButtonState {
+  initial,
+  loading,
+  disabled,
 }
--- lib/main.dart
+++ lib/main.dart
@@ -32,12 +32,433 @@ class Home extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            //Default
+            const FutureButton(
+              width: 200,
+              margin: EdgeInsets.all(12),
+              text: "Default",
+            ),
             FutureButton(
-              text: 'Submit',
-              onPressed: () {
-                print("Loaded");
+              width: 200,
+              margin: const EdgeInsets.all(12),
+              text: "OK",
+              onClick: () {},
+            ),
+            FutureButton(
+              width: 200,
+              margin: const EdgeInsets.all(12),
+              textState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return "Loading...";
+                  case ButtonState.disabled:
+                    return "Disabled";
+                  case ButtonState.initial:
+                    return "Submit";
+                }
               },
               onExecute: submit,
+            ),
+            //ExpendedIcon
+            FutureButton(
+              margin: const EdgeInsets.all(12),
+              width: 200,
+              expended: true,
+              iconPadding: const EdgeInsets.only(left: 16),
+              borderRadius: 25,
+              textState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return "Uploading...";
+                  case ButtonState.disabled:
+                    return "Disabled";
+                  case ButtonState.initial:
+                    return "Upload";
+                }
+              },
+              iconState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Icons.more_horiz;
+                  case ButtonState.disabled:
+                    return Icons.cancel;
+                  case ButtonState.initial:
+                    return Icons.upload;
+                }
+              },
+              colorState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber;
+                  case ButtonState.disabled:
+                    return Colors.red.shade200;
+                  case ButtonState.initial:
+                    return Colors.white;
+                }
+              },
+              backgroundState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber.shade50;
+                  case ButtonState.disabled:
+                    return Colors.red.shade50;
+                  case ButtonState.initial:
+                    return Colors.amber;
+                }
+              },
+              onClick: () {},
+            ),
+            FutureButton(
+              margin: const EdgeInsets.all(12),
+              width: 200,
+              expended: true,
+              iconAlignment: IconAlignment.start,
+              iconPadding: const EdgeInsets.only(right: 16),
+              borderRadius: 25,
+              textState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return "Uploading...";
+                  case ButtonState.disabled:
+                    return "Disabled";
+                  case ButtonState.initial:
+                    return "Upload";
+                }
+              },
+              iconState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Icons.more_horiz;
+                  case ButtonState.disabled:
+                    return Icons.cancel;
+                  case ButtonState.initial:
+                    return Icons.upload;
+                }
+              },
+              colorState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber;
+                  case ButtonState.disabled:
+                    return Colors.red.shade200;
+                  case ButtonState.initial:
+                    return Colors.white;
+                }
+              },
+              backgroundState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber.shade50;
+                  case ButtonState.disabled:
+                    return Colors.red.shade50;
+                  case ButtonState.initial:
+                    return Colors.amber;
+                }
+              },
+              onExecute: submit,
+            ),
+            FutureButton(
+              margin: const EdgeInsets.all(12),
+              width: 200,
+              expended: true,
+              iconPadding: const EdgeInsets.only(left: 16),
+              borderRadius: 25,
+              textState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return "Uploading...";
+                  case ButtonState.disabled:
+                    return "Disabled";
+                  case ButtonState.initial:
+                    return "Upload";
+                }
+              },
+              iconState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Icons.more_horiz;
+                  case ButtonState.disabled:
+                    return Icons.cancel;
+                  case ButtonState.initial:
+                    return Icons.upload;
+                }
+              },
+              colorState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber;
+                  case ButtonState.disabled:
+                    return Colors.red.shade200;
+                  case ButtonState.initial:
+                    return Colors.white;
+                }
+              },
+              backgroundState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber.shade50;
+                  case ButtonState.disabled:
+                    return Colors.red.shade50;
+                  case ButtonState.initial:
+                    return Colors.amber;
+                }
+              },
+            ),
+            //WithoutExpendedIcon
+            SizedBox(
+              width: 220,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FutureButton(
+                    iconSize: 24,
+                    borderRadius: 24,
+                    margin: const EdgeInsets.all(12),
+                    padding: const EdgeInsets.all(12),
+                    iconState: (state) {
+                      switch (state) {
+                        case ButtonState.loading:
+                          return Icons.more_horiz;
+                        case ButtonState.disabled:
+                          return Icons.cancel;
+                        case ButtonState.initial:
+                          return Icons.upload;
+                      }
+                    },
+                    colorState: (state) {
+                      switch (state) {
+                        case ButtonState.loading:
+                          return Colors.amber;
+                        case ButtonState.disabled:
+                          return Colors.red.shade200;
+                        case ButtonState.initial:
+                          return Colors.white;
+                      }
+                    },
+                    backgroundState: (state) {
+                      switch (state) {
+                        case ButtonState.loading:
+                          return Colors.amber.shade50;
+                        case ButtonState.disabled:
+                          return Colors.red.shade50;
+                        case ButtonState.initial:
+                          return Colors.amber;
+                      }
+                    },
+                    onClick: () {},
+                  ),
+                  FutureButton(
+                    iconSize: 24,
+                    borderRadius: 24,
+                    margin: const EdgeInsets.all(12),
+                    padding: const EdgeInsets.all(12),
+                    iconState: (state) {
+                      switch (state) {
+                        case ButtonState.loading:
+                          return Icons.more_horiz;
+                        case ButtonState.disabled:
+                          return Icons.cancel;
+                        case ButtonState.initial:
+                          return Icons.upload;
+                      }
+                    },
+                    colorState: (state) {
+                      switch (state) {
+                        case ButtonState.loading:
+                          return Colors.amber;
+                        case ButtonState.disabled:
+                          return Colors.red.shade200;
+                        case ButtonState.initial:
+                          return Colors.white;
+                      }
+                    },
+                    backgroundState: (state) {
+                      switch (state) {
+                        case ButtonState.loading:
+                          return Colors.amber.shade50;
+                        case ButtonState.disabled:
+                          return Colors.red.shade50;
+                        case ButtonState.initial:
+                          return Colors.amber;
+                      }
+                    },
+                    onExecute: submit,
+                  ),
+                  FutureButton(
+                    iconSize: 24,
+                    borderRadius: 24,
+                    margin: const EdgeInsets.all(12),
+                    padding: const EdgeInsets.all(12),
+                    iconState: (state) {
+                      switch (state) {
+                        case ButtonState.loading:
+                          return Icons.more_horiz;
+                        case ButtonState.disabled:
+                          return Icons.clear;
+                        case ButtonState.initial:
+                          return Icons.upload;
+                      }
+                    },
+                    colorState: (state) {
+                      switch (state) {
+                        case ButtonState.loading:
+                          return Colors.amber;
+                        case ButtonState.disabled:
+                          return Colors.red.shade200;
+                        case ButtonState.initial:
+                          return Colors.white;
+                      }
+                    },
+                    backgroundState: (state) {
+                      switch (state) {
+                        case ButtonState.loading:
+                          return Colors.amber.shade50;
+                        case ButtonState.disabled:
+                          return Colors.red.shade50;
+                        case ButtonState.initial:
+                          return Colors.amber;
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+            FutureButton(
+              margin: const EdgeInsets.all(12),
+              iconPadding: const EdgeInsets.only(left: 16),
+              borderRadius: 25,
+              textState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return "Uploading...";
+                  case ButtonState.disabled:
+                    return "Disabled";
+                  case ButtonState.initial:
+                    return "Upload";
+                }
+              },
+              iconState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Icons.more_horiz;
+                  case ButtonState.disabled:
+                    return Icons.cancel;
+                  case ButtonState.initial:
+                    return Icons.upload;
+                }
+              },
+              colorState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber;
+                  case ButtonState.disabled:
+                    return Colors.red.shade200;
+                  case ButtonState.initial:
+                    return Colors.white;
+                }
+              },
+              backgroundState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber.shade50;
+                  case ButtonState.disabled:
+                    return Colors.red.shade50;
+                  case ButtonState.initial:
+                    return Colors.amber;
+                }
+              },
+              onClick: () {},
+            ),
+            FutureButton(
+              margin: const EdgeInsets.all(12),
+              iconPadding: const EdgeInsets.only(left: 16),
+              borderRadius: 25,
+              textState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return "Uploading";
+                  case ButtonState.disabled:
+                    return "Disabled";
+                  case ButtonState.initial:
+                    return "Upload";
+                }
+              },
+              iconState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Icons.more_horiz;
+                  case ButtonState.disabled:
+                    return Icons.cancel;
+                  case ButtonState.initial:
+                    return Icons.upload;
+                }
+              },
+              colorState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber;
+                  case ButtonState.disabled:
+                    return Colors.red.shade200;
+                  case ButtonState.initial:
+                    return Colors.white;
+                }
+              },
+              backgroundState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber.shade50;
+                  case ButtonState.disabled:
+                    return Colors.red.shade50;
+                  case ButtonState.initial:
+                    return Colors.amber;
+                }
+              },
+              onExecute: submit,
+            ),
+            FutureButton(
+              margin: const EdgeInsets.all(12),
+              iconPadding: const EdgeInsets.only(left: 16),
+              borderRadius: 25,
+              textState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return "Uploading";
+                  case ButtonState.disabled:
+                    return "Disabled";
+                  case ButtonState.initial:
+                    return "Upload";
+                }
+              },
+              iconState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Icons.more_horiz;
+                  case ButtonState.disabled:
+                    return Icons.cancel;
+                  case ButtonState.initial:
+                    return Icons.upload;
+                }
+              },
+              colorState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber;
+                  case ButtonState.disabled:
+                    return Colors.red.shade200;
+                  case ButtonState.initial:
+                    return Colors.white;
+                }
+              },
+              backgroundState: (state) {
+                switch (state) {
+                  case ButtonState.loading:
+                    return Colors.amber.shade50;
+                  case ButtonState.disabled:
+                    return Colors.red.shade50;
+                  case ButtonState.initial:
+                    return Colors.amber;
+                }
+              },
             ),
           ],
         ),
@@ -45,11 +466,16 @@ class Home extends StatelessWidget {
     );
   }
 
-  Future<bool> submit() async {
+  Future submit() async {
     print("Loading...");
-    return Future.delayed(const Duration(seconds: 5)).then((value) {
+    await Future.delayed(const Duration(seconds: 5)).then((value) async {
+      print("Loaded 1");
+    });
+    await Future.delayed(const Duration(seconds: 5)).then((value) async {
+      print("Loaded 2");
+    });
+    await Future.delayed(const Duration(seconds: 5)).then((value) {
       print("Loaded");
-      return true;
     });
   }
 }
